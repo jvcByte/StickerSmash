9d324c6ced0862caef84c5aaf63330ebb1514ff2
jvcbyte <jvc8463@gmail.com>
2025-08-17 02:34:59 +0100

    feat: parsing properties, create customcomponents, install dependencies, extend styles by adding array of styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,20 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import ImageViewer from "../../components/ImageViewer";
+import Button from "../../components/Button";
+
+const icon = require("../../assets/images/icon.png");
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container} >
-      <Text style={styles.text}>StickerSmash!</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imageSrc={icon} />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Button label="Choose a Photo" theme="primary" />
+        <Button label="Share this photo" />
+      </View>
     </View>
   );
 }
@@ -15,19 +26,23 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "rgb(235, 255, 230)",
   },
-  text: {
-    fontSize: 20,
-    fontWeight: "bold",
+
+  imageContainer: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 18,
+    shadowColor: "rgba(0, 0, 0, 0.2)",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 6,
+    elevation: 6,
   },
-  link: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "white",
-    marginTop: 20,
-    paddingHorizontal: 35,
-    backgroundColor: "blue",
-    padding: 15,
-    borderRadius: 30,
+
+  buttonContainer: {
+    flex: 1 / 3,
     alignItems: "center",
     justifyContent: "center",
   },
